ecba1ecbdf20a1a41f1bd3ef418c68db4274e145
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2018-07-13 17:29:09 +0200

    Lint webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,69 +1,75 @@
-const path = require('path');
-const webpack = require('webpack');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
-const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
 
-const dev = process.env.NODE_ENV !== 'production';
+const dev = process.env.NODE_ENV !== "production";
 
 const HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
-  template: 'index.html',
-  filename: 'index.html',
-  inject: true,
+  template: "index.html",
+  filename: "index.html",
+  inject: true
 });
 
 const DefinePluginConfig = new webpack.DefinePlugin({
-  'process.env.NODE_ENV': JSON.stringify('production'),
+  "process.env.NODE_ENV": JSON.stringify("production")
 });
 
-const FaviconsWebpackPluginConfig = new FaviconsWebpackPlugin(path.join(__dirname, '/src/public/images/favicon-256.png'));
+const FaviconsWebpackPluginConfig = new FaviconsWebpackPlugin(
+  path.join(__dirname, "/src/public/images/favicon-256.png")
+);
 
 module.exports = {
   devServer: {
-    host: 'localhost',
-    port: '3000',
+    host: "localhost",
+    port: "3000",
     hot: true,
     headers: {
-      'Access-Control-Allow-Origin': '*',
+      "Access-Control-Allow-Origin": "*"
     },
-    historyApiFallback: true,
+    historyApiFallback: true
   },
   resolve: {
-    extensions: ['.js', '.ts', '.tsx'],
+    extensions: [".js", ".ts", ".tsx"],
     alias: {
-      'public': path.resolve(__dirname, 'src/public/')
+      public: path.resolve(__dirname, "src/public/")
     }
   },
-  entry: ['react-hot-loader/patch', path.join(__dirname, '/src/index.tsx')],
+  entry: ["react-hot-loader/patch", path.join(__dirname, "/src/index.tsx")],
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        loader: 'babel-loader',
+        loader: "babel-loader"
       },
       {
-        enforce: 'pre',
+        enforce: "pre",
         test: /\.js$/,
-        loader: 'source-map-loader',
+        loader: "source-map-loader"
       },
       {
         test: /\.scss$/,
-        loader: 'style-loader!css-loader!sass-loader',
+        loader: "style-loader!css-loader!sass-loader"
       },
       {
         test: /\.(jpe?g|png|gif|svg)$/i,
-        loader: 'url-loader',
+        loader: "url-loader",
         options: {
-          limit: 10000,
-        },
-      },
-    ],
+          limit: 10000
+        }
+      }
+    ]
   },
   output: {
-    filename: 'index.js',
-    path: path.join(__dirname, '/build'),
+    filename: "index.js",
+    path: path.join(__dirname, "/build")
   },
-  mode: dev ? 'development' : 'production',
+  mode: dev ? "development" : "production",
   plugins: dev
-    ? [FaviconsWebpackPluginConfig, HTMLWebpackPluginConfig, new webpack.HotModuleReplacementPlugin()]
-    : [FaviconsWebpackPluginConfig, HTMLWebpackPluginConfig, DefinePluginConfig],
+    ? [
+        FaviconsWebpackPluginConfig,
+        HTMLWebpackPluginConfig,
+        new webpack.HotModuleReplacementPlugin()
+      ]
+    : [FaviconsWebpackPluginConfig, HTMLWebpackPluginConfig, DefinePluginConfig]
 };
