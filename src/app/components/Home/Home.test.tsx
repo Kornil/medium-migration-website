bfa1b5e11ef9811a66827785fc131e4d435d6496
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2018-07-15 17:46:05 +0200

    Test props

--- src/app/components/Home/Home.test.tsx
+++ src/app/components/Home/Home.test.tsx
@@ -1,9 +1,21 @@
+import { mount } from "enzyme";
 import React from "react";
 import { render, unmountComponentAtNode } from "react-dom";
+
 import Home from "./Home";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
-  render(<Home user="Default User" />, div);
+  render(<Home />, div);
   unmountComponentAtNode(div);
+});
+
+it("renders correct user prop when supplied", () => {
+  const wrapper = mount(<Home user="Francesco" />);
+  expect(wrapper.prop("user")).toEqual("Francesco");
+});
+
+it("renders correct default prop when not supplied", () => {
+  const wrapper = mount(<Home />);
+  expect(wrapper.prop("user")).toEqual("Default User");
 });
