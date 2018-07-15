37b998b5cc284fd2ed58ec33e3db47b1f69ce64d
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2018-07-15 11:10:27 +0200

    Extract jest config into own file

---
+++ jest.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  moduleDirectories: ["node_modules", "."],
+  setupFiles: ["<rootDir>/src/tests/setup.ts"],
+  moduleNameMapper: {
+    "\\.(css|styl|less|sass|scss)$": "identity-obj-proxy",
+    "^public/(.*)$": "<rootDir>/src/tests/fileTransformer.js",
+    "^app/(.*)$": "<rootDir>/src/app/$1"
+  },
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+  transform: {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  snapshotSerializers: ["enzyme-to-json/serializer"],
+  setupTestFrameworkScriptFile: "<rootDir>/src/tests/setup.ts",
+  collectCoverage: true,
+  coverageDirectory: "./coverage/",
+  collectCoverageFrom: ["**/*.{ts,tsx}"],
+  coveragePathIgnorePatterns: ["/node_modules/", "/custom-types/"],
+  globals: {
+    "ts-jest": {
+      tsConfigFile: "tsconfig.json",
+      useBabelrc: true
+    }
+  }
+};
