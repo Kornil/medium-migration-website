487a1c6ed3c4dd6560ccffdc1b0a34051fbfa2f0
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2018-07-28 16:44:30 +0200

    Re-enable webpack favicon plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const webpack = require("webpack");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
+const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
 
 const dev = process.env.NODE_ENV !== "production";
 
@@ -13,6 +14,10 @@ const HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
 const DefinePluginConfig = new webpack.DefinePlugin({
   "process.env.NODE_ENV": JSON.stringify("production")
 });
+
+const FaviconsWebpackPluginConfig = new FaviconsWebpackPlugin(
+  "public/images/favicon-256.png"
+);
 
 module.exports = {
   context: path.join(__dirname, "src"),
@@ -63,6 +68,10 @@ module.exports = {
   },
   mode: dev ? "development" : "production",
   plugins: dev
-    ? [HTMLWebpackPluginConfig, new webpack.HotModuleReplacementPlugin()]
-    : [HTMLWebpackPluginConfig, DefinePluginConfig]
+    ? [
+        HTMLWebpackPluginConfig,
+        FaviconsWebpackPluginConfig,
+        new webpack.HotModuleReplacementPlugin()
+      ]
+    : [HTMLWebpackPluginConfig, FaviconsWebpackPluginConfig, DefinePluginConfig]
 };
