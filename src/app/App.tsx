5db589c4d2231ae2bf6c3d094054d4d98c75ed42
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2018-07-29 18:05:34 +0200

    Add styled comp files to main app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,16 +5,20 @@ import { BrowserRouter, Link } from "react-router-dom";
 
 import Routes from "./Routes";
 
+import Header from "app/styled-components/Header";
+import PageLayout from "app/styled-components/PageLayout";
+
 import reactLogo from "public/images/React-icon.png";
 
 export const App: React.SFC<{}> = () => (
   <BrowserRouter>
-    <main className="container">
-      <div>
+    <PageLayout>
+      <Header>
         <h1>hello world!</h1>
-        <img className="container__image" alt="react logo" src={reactLogo} />
-        <p>If you see this everything is working!</p>
-      </div>
+      </Header>
+      <h2>asd</h2>
+      <img className="container__image" alt="react logo" src={reactLogo} />
+      <p>If you see this everything is working!</p>
       <ul className="left">
         <li>
           <Link to="/">Home</Link>
@@ -24,6 +28,6 @@ export const App: React.SFC<{}> = () => (
         </li>
       </ul>
       <Routes />
-    </main>
+    </PageLayout>
   </BrowserRouter>
 );
