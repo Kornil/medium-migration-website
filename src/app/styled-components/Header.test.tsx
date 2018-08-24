8486fd4ce10d28b9c59fd89ab9f07dce62159dc8
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2018-08-24 22:32:26 +0200

    Add snapshots for styles

--- src/app/styled-components/Header.test.tsx
+++ src/app/styled-components/Header.test.tsx
@@ -1,9 +1,9 @@
 import { shallow } from "enzyme";
 import React from "react";
 
-import PageLayout from "./PageLayout";
+import Header from "./Header";
 
 test("it works", () => {
-  const wrapper = shallow(<PageLayout />);
+  const wrapper = shallow(<Header />);
   expect(wrapper).toMatchSnapshot();
 });
