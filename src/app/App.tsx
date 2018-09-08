d2fb888b9a5d19cab14c3c9bd138deaa3ad191d2
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2018-09-08 21:37:07 +0200

    Remove lorem ipsum

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,23 +5,16 @@ import { BrowserRouter } from "react-router-dom";
 
 import Routes from "./Routes";
 
-import { Header } from "app/components/shared";
+import { Cta, Header } from "app/components/shared";
 import { PageLayout } from "app/styled-components";
 
-import reactLogo from "public/images/React-icon.png";
+// import reactLogo from "public/images/React-icon.png";
 
 export const App: React.SFC<{}> = () => (
   <BrowserRouter>
     <PageLayout>
       <Header />
-      <h2>asd</h2>
-      <img
-        style={{ width: "100px" }}
-        className="container__image"
-        alt="react logo"
-        src={reactLogo}
-      />
-      <p>If you see this everything is working!</p>
+      <Cta />
       <Routes />
     </PageLayout>
   </BrowserRouter>
