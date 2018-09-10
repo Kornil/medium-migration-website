5e063e7fcfb80039469c623a1fad40b44a8dfd1b
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2018-09-10 23:16:29 +0200

    Remove unused markup

--- src/app/components/Home/Home.tsx
+++ src/app/components/Home/Home.tsx
@@ -4,12 +4,7 @@ interface Homeprops {
   user?: string;
 }
 
-const Home: React.SFC<Homeprops> = ({ user }) => (
-  <div>
-    <h2>Home Page</h2>
-    <p>Hello {user}</p>
-  </div>
-);
+const Home: React.SFC<Homeprops> = ({ user }) => <div />;
 
 Home.defaultProps = {
   user: "Default User"
