b308803b08c016d3893c9a8003ec3425794df081
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2018-09-15 00:17:50 +0200

    Fix typecheck

--- src/app/components/Home/Home.tsx
+++ src/app/components/Home/Home.tsx
@@ -4,7 +4,7 @@ interface Homeprops {
   user?: string;
 }
 
-const Home: React.SFC<Homeprops> = ({ user }) => <div />;
+const Home: React.SFC<Homeprops> = ({ user }) => <div className={user} />;
 
 Home.defaultProps = {
   user: "Default User"
