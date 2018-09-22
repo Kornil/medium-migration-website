9f92f7b2351a8c5ca93fc51664d9f876828552da
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2018-09-22 14:01:05 +0200

    Remove unused prop

--- src/app/Routes.tsx
+++ src/app/Routes.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, RouteProps, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 import { About, Home } from "app/components";
 
