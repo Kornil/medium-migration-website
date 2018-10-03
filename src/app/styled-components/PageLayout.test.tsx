b733c7c795a20e8f5c1bb1c26d2b16825d3279a8
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2018-10-03 09:27:39 +0200

    Remove superflous tests

--- src/app/styled-components/PageLayout.test.tsx
+++
@@ -1,9 +0,0 @@
-import { shallow } from "enzyme";
-import React from "react";
-
-import PageLayout from "./PageLayout";
-
-test("it works", () => {
-  const wrapper = shallow(<PageLayout />);
-  expect(wrapper).toMatchSnapshot();
-});
--- src/app/styled-components/StyledHeader.test.tsx
+++
@@ -1,9 +0,0 @@
-import { shallow } from "enzyme";
-import React from "react";
-
-import StyledHeader from "./StyledHeader";
-
-test("it works", () => {
-  const wrapper = shallow(<StyledHeader />);
-  expect(wrapper).toMatchSnapshot();
-});
