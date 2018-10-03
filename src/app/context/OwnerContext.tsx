b4ec49767c27f3139963e8e00b1f7d325c55e89a
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2018-10-04 00:02:44 +0200

    Add social fields to ownerContext

--- src/app/context/OwnerContext.tsx
+++ src/app/context/OwnerContext.tsx
@@ -1,11 +1,21 @@
 import createContextFactory from "./createContext";
 
-interface OwnerContextValueInterface {
+export interface OwnerContextValueInterface {
   name: string;
+  socials: {
+    github: string;
+    linkedin: string;
+    twitter: string;
+  };
 }
 
 export const ownerContextValue: OwnerContextValueInterface = {
-  name: "Francesco Agnoletto"
+  name: "Francesco Agnoletto",
+  socials: {
+    github: "https://github.com/Kornil",
+    linkedin: "https://www.linkedin.com/in/francesco-agnoletto-176171114/",
+    twitter: "https://twitter.com/Fragno92"
+  }
 };
 
 const OwnerContext = createContextFactory("Owner", ownerContextValue);
