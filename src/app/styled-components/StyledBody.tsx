051264f088e03fbbd64267d25ea1e2c8570a27ce
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2018-10-03 15:22:30 +0200

    Add more styled components

---
+++ src/app/styled-components/StyledBody.tsx
@@ -0,0 +1,8 @@
+import styled from "react-emotion";
+
+const StyledBody = styled.div`
+  padding: 16px 8%;
+  text-align: start;
+`;
+
+export default StyledBody;
--- src/app/styled-components/index.ts
+++ src/app/styled-components/index.ts
@@ -1,4 +1,6 @@
 export { default as StyledHeader } from "app/styled-components/StyledHeader";
 export { default as PageLayout } from "./PageLayout";
 export { default as NavLink } from "./NavLink";
-export { default as StyledLink } from "./StyledLink";
+export { default as StyledBody } from "./StyledBody";
+export { default as StyledTitle } from "./StyledTitle";
+export { default as StyledEm } from "./StyledEm";
