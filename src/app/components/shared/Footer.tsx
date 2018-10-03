8bf5acf752fbcf5b7399540736d6ef13fc4da632
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2018-10-03 18:38:58 +0200

    Add icons to footer

--- src/app/components/shared/Footer.tsx
+++ src/app/components/shared/Footer.tsx
@@ -1,17 +1,32 @@
 import React from "react";
 import styled from "react-emotion";
 
+import { StyledLink } from "app/styled-components";
+import GithubLogo from "public/github-brands.svg";
+import LinkedinLogo from "public/linkedin-in-brands.svg";
+import TwitterLogo from "public/twitter-brands.svg";
+
 const StyledFooter = styled.footer`
   display: flex;
   align-items: center;
   justify-content: center;
+
+  a {
+    padding: 0 48px;
+  }
 `;
 
 const Footer = () => (
   <StyledFooter>
-    <p>a</p>
-    <p>b</p>
-    <p>c</p>
+    <StyledLink href="">
+      <GithubLogo width="25px" height="25px" />
+    </StyledLink>
+    <StyledLink href="">
+      <LinkedinLogo width="25px" height="25px" />
+    </StyledLink>
+    <StyledLink href="">
+      <TwitterLogo width="25px" height="25px" />
+    </StyledLink>
   </StyledFooter>
 );
 
