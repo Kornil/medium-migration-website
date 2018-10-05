2178a41b54cda531d2c896f56bc8b2e00089e23f
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2018-10-05 18:23:40 +0200

    Revert code split

--- src/app/Routes.tsx
+++ src/app/Routes.tsx
@@ -1,12 +1,10 @@
 import React from "react";
-import Loadable from "react-loadable";
 import { Route, Switch } from "react-router-dom";
 
 import { Blog, Home } from "app/components";
 
 interface RoutesInterface {
   Component: React.SFC<any> | typeof React.Component;
-  name: string;
   path: string;
 }
 
@@ -14,33 +12,17 @@ interface RoutesInterface {
 export const routes: RoutesInterface[] = [
   {
     Component: Home,
-    name: "Home",
     path: "/"
   },
   {
     Component: Blog,
-    name: "Blog",
     path: "/blog"
   }
 ];
 
 const Routes: React.SFC<{}> = () => {
-  const routeComponents = routes.map(({ path, name, Component }, key) => {
-    // @ts-ignore
-    const LoadableComponent = Loadable({
-      loader: () => import(`app/components/${name}`),
-      loading: <div>Loading</div>
-    });
-    return (
-      <Route
-        exact
-        path={path}
-        component={
-          process.env.NODE_ENV !== "production" ? Component : LoadableComponent
-        }
-        key={key}
-      />
-    );
+  const routeComponents = routes.map(({ path, Component }, key) => {
+    return <Route exact path={path} component={Component} key={key} />;
   });
   return <Switch>{routeComponents}</Switch>;
 };
