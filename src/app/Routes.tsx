ad577014c8e2d02aa64e79c85b0a2e2e24e91792
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2018-10-05 18:05:55 +0200

    Add code splitting

--- src/app/Routes.tsx
+++ src/app/Routes.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Loadable from "react-loadable";
 import { Route, Switch } from "react-router-dom";
 
 import { Blog, Home } from "app/components";
 
 interface RoutesInterface {
   Component: React.SFC<any> | typeof React.Component;
+  name: string;
   path: string;
 }
 
@@ -12,18 +14,32 @@ interface RoutesInterface {
 export const routes: RoutesInterface[] = [
   {
     Component: Home,
+    name: "Home",
     path: "/"
   },
   {
     Component: Blog,
+    name: "Blog",
     path: "/blog"
   }
 ];
 
 const Routes: React.SFC<{}> = () => {
-  const routeComponents = routes.map(({ path, Component }, key) => (
-    <Route exact path={path} component={Component} key={key} />
-  ));
+  const routeComponents = routes.map(({ path, name, Component }, key) => {
+    // @ts-ignore
+    const LoadableComponent = Loadable({
+      loader: () => import(`app/components/${name}`),
+      loading: <div>Loading</div>
+    });
+    return (
+      <Route
+        exact
+        path={path}
+        component={module.hot ? Component : LoadableComponent}
+        key={key}
+      />
+    );
+  });
   return <Switch>{routeComponents}</Switch>;
 };
 
