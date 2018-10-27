9ac72de31165481f88af879a350016141b8d733d
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2018-10-27 20:34:51 +0200

    Use Cta on 404 page

--- src/client/app/components/NotFound.tsx
+++ src/client/app/components/NotFound.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 
-import { RouteWithStatus } from "app/components/shared";
+import { Cta, RouteWithStatus } from "app/components/shared";
 
 const NotFound: React.SFC<{}> = () => (
   <RouteWithStatus code={404}>
-    <p>404 - not found</p>
+    <Cta
+      title="404 - Not Found"
+      body={<p>This is not the web page you are looking for.</p>}
+    />
   </RouteWithStatus>
 );
 
