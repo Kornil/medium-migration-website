71d66d40af4fd2955562395b8bccaccab497bbaa
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2018-10-29 17:10:52 +0100

    Add basic match functionality to Story

--- src/client/app/components/Story/index.tsx
+++ src/client/app/components/Story/index.tsx
@@ -1,5 +1,8 @@
 import React from "react";
+import { match } from "react-router-dom";
 
-const Story: React.SFC<{ story: any }> = ({ story }) => <p>{story.title}</p>;
+const Story: React.SFC<{ match: match<{ id: string }> }> = props => (
+  <p>{props.match.params.id}</p>
+);
 
 export default Story;
