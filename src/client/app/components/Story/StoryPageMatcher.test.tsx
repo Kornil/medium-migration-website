ca1f175f6bf32ac78ae73d07d4248e31fdaedaf3
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2018-10-30 00:15:43 +0100

    Add tests for StoryPageMatcher

---
+++ src/client/app/components/Story/StoryPageMatcher.test.tsx
@@ -0,0 +1,37 @@
+import { shallow } from "enzyme";
+import React from "react";
+import { render, unmountComponentAtNode } from "react-dom";
+
+import { NotFound } from "app/components";
+
+import { StoryPageMatcher } from "./";
+
+const fakeStory = {
+  link: "https://google.com",
+  publishedAt: 1529091302770,
+  title: "hello"
+};
+
+describe("<StoryPageMatcher />", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    render(
+      <StoryPageMatcher
+        StoriesContext={{ stories: [fakeStory] }}
+        match={{ params: { id: "hello" }, isExact: true, path: "", url: "" }}
+      />,
+      div
+    );
+    unmountComponentAtNode(div);
+  });
+
+  it("renders 404 if invalid :id", () => {
+    const wrapper = shallow(
+      <StoryPageMatcher
+        StoriesContext={{ stories: [fakeStory] }}
+        match={{ params: { id: "wrong" }, isExact: true, path: "", url: "" }}
+      />
+    );
+    expect(wrapper.find(NotFound)).toHaveLength(1);
+  });
+});
