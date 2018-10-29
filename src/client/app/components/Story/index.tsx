95a353b216302288fffb303deea7c121e5eb9da6
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2018-10-30 00:14:57 +0100

    Refactor Story initial file into matcher for Story page

--- src/client/app/components/Story/index.tsx
+++ src/client/app/components/Story/index.tsx
@@ -1,8 +1,23 @@
 import React from "react";
 import { match } from "react-router-dom";
 
-const Story: React.SFC<{ match: match<{ id: string }> }> = props => (
-  <p>{props.match.params.id}</p>
-);
+import { NotFound } from "app/components";
+import StoriesContext, { StoryInterface } from "app/context/StoriesContext";
 
-export default Story;
+interface StoryPageProps {
+  match: match<{ id: string }>;
+  StoriesContext: { stories: StoryInterface[] };
+}
+
+export const StoryPageMatcher: React.SFC<StoryPageProps> = props => {
+  if (props.StoriesContext.stories && props.StoriesContext.stories.length) {
+    const filteredStory = props.StoriesContext.stories.find(
+      story => story.title === props.match.params.id
+    );
+    return filteredStory ? <p>{props.match.params.id}</p> : <NotFound />;
+  } else {
+    return null;
+  }
+};
+
+export default StoriesContext.connect(StoryPageMatcher);
