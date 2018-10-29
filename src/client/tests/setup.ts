1943477014c8a61ccb6ac415431a0fca2682d7b5
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2018-10-29 11:15:25 +0100

    Add jest-localstorage-mock for localstore tests

--- src/client/tests/setup.ts
+++ src/client/tests/setup.ts
@@ -4,6 +4,8 @@ import Adapter from "enzyme-adapter-react-16";
 import * as emotion from "emotion";
 import { createSerializer } from "jest-emotion";
 
+import "jest-localstorage-mock";
+
 expect.addSnapshotSerializer(createSerializer(emotion));
 
 configure({ adapter: new Adapter() });
