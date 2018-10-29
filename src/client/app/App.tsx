abcc4d391c78a8b876adde7bd6517657565f8e3d
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2018-10-29 11:13:35 +0100

    Add StoriesContext to <App />
    This makes webapp more efficient by loading data on background as soon as you open the website, instead of when visiting /blog page

--- src/client/app/App.tsx
+++ src/client/app/App.tsx
@@ -1,23 +1,43 @@
 // <reference path='./index.d.ts'/>
 
-import React from "react";
+import React, { Component } from "react";
 
 import Routes from "app/Routes";
 
 import { Footer, Header } from "app/components/shared";
-import OwnerContext, { ownerContextValue } from "app/context/OwnerContext";
 import { PageLayout } from "app/styled-components";
+
+import OwnerContext, { ownerContextValue } from "app/context/OwnerContext";
+import StoriesContext, { storiesContext } from "app/context/StoriesContext";
 
 // import reactLogo from "assets/images/React-icon.png";
 
-const App: React.SFC<{}> = () => (
-  <OwnerContext.Provider value={ownerContextValue}>
-    <PageLayout>
-      <Header />
-      <Routes />
-      <Footer />
-    </PageLayout>
-  </OwnerContext.Provider>
-);
+class App extends Component {
+  state = {
+    articlesData: { stories: [] }
+  };
+
+  async componentDidMount() {
+    const articlesData = await storiesContext();
+    this.setState({
+      articlesData
+    });
+  }
+
+  render() {
+    const { articlesData } = this.state;
+    return (
+      <OwnerContext.Provider value={ownerContextValue}>
+        <PageLayout>
+          <Header />
+          <StoriesContext.Provider value={articlesData}>
+            <Routes />
+          </StoriesContext.Provider>
+          <Footer />
+        </PageLayout>
+      </OwnerContext.Provider>
+    );
+  }
+}
 
 export default App;
