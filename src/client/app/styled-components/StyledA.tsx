bd122c578a7839b4a41e7a189527603e926f2248
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2018-10-30 18:14:42 +0100

    Introduce StyledA

---
+++ src/client/app/styled-components/StyledA.tsx
@@ -0,0 +1,12 @@
+import styled from "react-emotion";
+
+const StyledA = styled.a`
+  color: #3d3d3d;
+  text-decoration: none;
+
+  :hover {
+    color: #1f1f1f;
+  }
+`;
+
+export default StyledA;
--- src/client/app/styled-components/index.ts
+++ src/client/app/styled-components/index.ts
@@ -4,4 +4,5 @@ export { default as StyledBody } from "./StyledBody";
 export { default as StyledTitle } from "./StyledTitle";
 export { default as StyledEm } from "./StyledEm";
 export { default as StyledLink } from "./StyledLink";
+export { default as StyledA } from "./StyledA";
 export { default as StyledLoading } from "./StyledLoading";
