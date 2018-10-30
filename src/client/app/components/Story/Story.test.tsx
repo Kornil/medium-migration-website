40637b16d589deeefa61bf8d6ee01e8103886896
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2018-10-31 00:21:19 +0100

    Fix Story test

--- src/client/app/components/Story/Story.test.tsx
+++ src/client/app/components/Story/Story.test.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { render, unmountComponentAtNode } from "react-dom";
 
-import Story from "./";
+import Story from "./Story";
+
+const story = {
+  value: {
+    title: "hello"
+  }
+};
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
-  render(<Story />, div);
+  render(<Story story={story} />, div);
   unmountComponentAtNode(div);
 });
