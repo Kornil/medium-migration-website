5818391113bd3acf087aedca8bc4718447c01050
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2018-11-01 22:44:19 +0100

    Draft story page

--- src/client/app/containers/Story/Story.tsx
+++ src/client/app/containers/Story/Story.tsx
@@ -1,7 +1,22 @@
 import React from "react";
 
-const Story: React.SFC<{ story: any }> = props => (
-  <p>{props.story.value.title}</p>
-);
+import { Editor } from "app/containers";
+import { StyledBody } from "app/styled-components";
+
+import { RightSidebar } from "./components";
+
+const Story: React.SFC<{ story: any }> = props => {
+  const { story } = props;
+  return (
+    <StyledBody>
+      <p>me</p>
+      <div>
+        <h1>{story.title}</h1>
+        <Editor mediumValue={story.content} />
+      </div>
+      <RightSidebar date={story.firstPublishedAt} />
+    </StyledBody>
+  );
+};
 
 export default Story;
