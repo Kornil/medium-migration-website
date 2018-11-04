a7595a2c433a4b9a6b9c36dd00221f10edc56323
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2018-11-04 10:00:21 +0100

    Use defined const instead of loose strings

--- src/client/app/containers/Editor/RichTextPlugin.tsx
+++ src/client/app/containers/Editor/RichTextPlugin.tsx
@@ -1,25 +1,32 @@
 import React from "react";
 import { Plugin } from "slate-react";
 
+import { BLOCKS } from "./constants";
+
 type RichTextPluginInterface = () => Plugin;
 
 const RichTextPlugin: RichTextPluginInterface = () => {
   return {
     renderNode: (props, editor, next) => {
       const { attributes, children, node } = props;
-      switch (node.object === "block" || node.object === "inline" && node.type) {
-        case "block-quote":
+      switch (node.type) {
+        case BLOCKS.BLOCK_QUOTE:
           return <blockquote {...attributes}>{children}</blockquote>;
-        case "bulleted-list":
+        case BLOCKS.BULLETED_LIST:
           return <ul {...attributes}>{children}</ul>;
-        case "heading-one":
+        case BLOCKS.HEADING_ONE:
           return <h1 {...attributes}>{children}</h1>;
-        case "heading-two":
+        case BLOCKS.HEADING_TWO:
           return <h2 {...attributes}>{children}</h2>;
+        case BLOCKS.HEADING_THREE:
+          return <h3 {...attributes}>{children}</h3>;
         case "list-item":
+        case BLOCKS.LIST_ITEM:
           return <li {...attributes}>{children}</li>;
-        case "numbered-list":
+        case BLOCKS.NUMBERED_LIST:
           return <ol {...attributes}>{children}</ol>;
+        case BLOCKS.PARAGRAPH:
+          return <p {...attributes}>{children}</p>;
         default:
           return next();
       }
---
+++ src/client/app/containers/Editor/constants.ts
@@ -0,0 +1,11 @@
+export enum BLOCKS {
+  BLOCK_QUOTE = "block-quote",
+  BULLETED_LIST = "bulleted-list",
+  HEADING_ONE = "heading-one",
+  HEADING_TWO = "heading-two",
+  HEADING_THREE = "heading-three",
+  IMAGE = "image",
+  LIST_ITEM = "list-item",
+  NUMBERED_LIST = "numbered-list",
+  PARAGRAPH = "paragraph"
+}
