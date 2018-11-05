694b1a8ebf63d3ded3fb3a7f18ea19b4c2b0d3cd
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2018-11-05 14:03:04 +0100

    Fix unreachable case when editor could be undefined

--- src/client/app/containers/Editor/index.tsx
+++ src/client/app/containers/Editor/index.tsx
@@ -72,81 +72,84 @@ class EditorWrapper extends Component<EditorWrapperProps, EditorWrapperState> {
   MediumToSlateConverter = () => {
     const { paragraphs } = this.props.mediumValue.bodyModel;
     const { editor } = this;
-    paragraphs.forEach((block: any, i: number) => {
-      switch (block.type) {
-        case 1:
-          const paragraph = Block.fromJSON(
-            createBlock(BLOCKS.PARAGRAPH, block.text)
-          );
-          editor.insertBlock(paragraph);
+    if (editor) {
+      paragraphs.forEach((block: any, i: number) => {
+        switch (block.type) {
+          case 1:
+            const paragraph = Block.fromJSON(
+              createBlock(BLOCKS.PARAGRAPH, block.text)
+            );
+            editor.insertBlock(paragraph);
 
-          // Add markup logic
-          if (block.markups.length) {
-            const firstNode = paragraph.nodes.first();
-            block.markups.forEach((mark: any) => {
-              const range = Range.fromJSON({
-                anchor: {
-                  key: firstNode.key,
-                  object: "point",
-                  offset: mark.start
-                },
-                focus: {
-                  key: firstNode.key,
-                  object: "point",
-                  offset: mark.end
-                }
+            // Add markup logic
+            if (block.markups.length) {
+              const firstNode = paragraph.nodes.first();
+              block.markups.forEach((mark: any) => {
+                const range = Range.fromJSON({
+                  anchor: {
+                    key: firstNode.key,
+                    object: "point",
+                    offset: mark.start
+                  },
+                  focus: {
+                    key: firstNode.key,
+                    object: "point",
+                    offset: mark.end
+                  }
+                });
+                const type = findMarkType(mark);
+                editor.addMarkAtRange(range, type);
               });
-              const type = findMarkType(mark);
-              editor.addMarkAtRange(range, type);
+            }
+            break;
+          case 3:
+            const headingOne = Block.fromJSON(
+              createBlock(BLOCKS.HEADING_ONE, block.text)
+            );
+            // logic for removing first editor block (empty paragraph) for title
+            if (i === 0) {
+              editor.setBlocks(BLOCKS.HEADING_ONE);
+              editor.insertText(block.text);
+            } else {
+              editor.insertBlock(headingOne);
+            }
+            break;
+          case 13:
+            if (i === 1) {
+              const headingTwo = Block.fromJSON(
+                createBlock(BLOCKS.HEADING_TWO, block.text)
+              );
+              editor.insertBlock(headingTwo);
+            } else {
+              const headingThree = Block.fromJSON(
+                createBlock(BLOCKS.HEADING_THREE, block.text)
+              );
+              editor.insertBlock(headingThree);
+            }
+            break;
+          case 8:
+            const blockQuote = Block.fromJSON(
+              createBlock(BLOCKS.BLOCK_QUOTE, block.text)
+            );
+            editor.insertBlock(blockQuote);
+            break;
+          case 4:
+            const image = Block.create({
+              data: {
+                isMainImage: i === 2,
+                name: block.name,
+                src:
+                  "https://cdn-images-1.medium.com/max/1280/" +
+                  block.metadata.id,
+                text: block.text
+              },
+              type: BLOCKS.IMAGE
             });
-          }
-          break;
-        case 3:
-          const headingOne = Block.fromJSON(
-            createBlock(BLOCKS.HEADING_ONE, block.text)
-          );
-          // logic for removing first editor block (empty paragraph) for title
-          if (i === 0) {
-            editor.setBlocks(BLOCKS.HEADING_ONE);
-            editor.insertText(block.text);
-          } else {
-            editor.insertBlock(headingOne);
-          }
-          break;
-        case 13:
-          if (i === 1) {
-            const headingTwo = Block.fromJSON(
-              createBlock(BLOCKS.HEADING_TWO, block.text)
-            );
-            editor.insertBlock(headingTwo);
-          } else {
-            const headingThree = Block.fromJSON(
-              createBlock(BLOCKS.HEADING_THREE, block.text)
-            );
-            editor.insertBlock(headingThree);
-          }
-          break;
-        case 8:
-          const blockQuote = Block.fromJSON(
-            createBlock(BLOCKS.BLOCK_QUOTE, block.text)
-          );
-          editor.insertBlock(blockQuote);
-          break;
-        case 4:
-          const image = Block.create({
-            data: {
-              isMainImage: i === 2,
-              name: block.name,
-              src:
-                "https://cdn-images-1.medium.com/max/1280/" + block.metadata.id,
-              text: block.text
-            },
-            type: BLOCKS.IMAGE
-          });
-          editor.insertBlock(image);
-          break;
-      }
-    });
+            editor.insertBlock(image);
+            break;
+        }
+      });
+    }
   };
 
   render() {
