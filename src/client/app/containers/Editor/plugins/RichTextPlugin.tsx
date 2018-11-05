842f45baf93af3be7bddacb74fa9567571c3593c
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2018-11-05 23:30:18 +0100

    Temporary fix type issue

--- src/client/app/containers/Editor/plugins/RichTextPlugin.tsx
+++ src/client/app/containers/Editor/plugins/RichTextPlugin.tsx
@@ -9,37 +9,45 @@ const RichTextPlugin: RichTextPluginInterface = () => {
   return {
     renderNode: (props, {}, next) => {
       const { attributes, children, node } = props;
-      switch (node.type) {
-        case BLOCKS.BLOCK_QUOTE:
-          return <blockquote {...attributes}>{children}</blockquote>;
-        case BLOCKS.BULLETED_LIST:
-          return <ul {...attributes}>{children}</ul>;
-        case BLOCKS.HEADING_ONE:
-          return <h1 {...attributes}>{children}</h1>;
-        case BLOCKS.HEADING_TWO:
-          return <h2 {...attributes}>{children}</h2>;
-        case BLOCKS.HEADING_THREE:
-          return <h3 {...attributes}>{children}</h3>;
-        case BLOCKS.IMAGE:
-          const { data } = node;
-          return (
-            <figure {...attributes}>
-              <img
-                src={data.get("src")}
-                alt={data.get("name")}
-                style={{ maxWidth: "100%", display: "block", margin: "0 auto" }}
-              />
-              <figcaption>{data.get("text")}</figcaption>
-            </figure>
-          );
-        case BLOCKS.LIST_ITEM:
-          return <li {...attributes}>{children}</li>;
-        case BLOCKS.NUMBERED_LIST:
-          return <ol {...attributes}>{children}</ol>;
-        case BLOCKS.PARAGRAPH:
-          return <p {...attributes}>{children}</p>;
-        default:
-          return next();
+      // here until https://github.com/DefinitelyTyped/DefinitelyTyped/pull/30186/ gets sorted
+      // istanbul ignore next
+      if (node.object === "block" || node.object === "inline") {
+        switch (node.type) {
+          case BLOCKS.BLOCK_QUOTE:
+            return <blockquote {...attributes}>{children}</blockquote>;
+          case BLOCKS.BULLETED_LIST:
+            return <ul {...attributes}>{children}</ul>;
+          case BLOCKS.HEADING_ONE:
+            return <h1 {...attributes}>{children}</h1>;
+          case BLOCKS.HEADING_TWO:
+            return <h2 {...attributes}>{children}</h2>;
+          case BLOCKS.HEADING_THREE:
+            return <h3 {...attributes}>{children}</h3>;
+          case BLOCKS.IMAGE:
+            const { data } = node;
+            return (
+              <figure {...attributes}>
+                <img
+                  src={data.get("src")}
+                  alt={data.get("name")}
+                  style={{
+                    display: "block",
+                    margin: "0 auto",
+                    maxWidth: "100%"
+                  }}
+                />
+                <figcaption>{data.get("text")}</figcaption>
+              </figure>
+            );
+          case BLOCKS.LIST_ITEM:
+            return <li {...attributes}>{children}</li>;
+          case BLOCKS.NUMBERED_LIST:
+            return <ol {...attributes}>{children}</ol>;
+          case BLOCKS.PARAGRAPH:
+            return <p {...attributes}>{children}</p>;
+          default:
+            return next();
+        }
       }
     },
 
