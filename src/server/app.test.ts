4ce7368029058b0d1f3384d846d537f8b9c92084
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2018-11-07 20:38:08 +0100

    ForceExit jest
    No issues locally, but travisci freezes after test run successfully

--- src/server/app.test.ts
+++ src/server/app.test.ts
@@ -1,12 +1,8 @@
-import mongoose from "mongoose";
 import request from "supertest";
 
 import app from "./app";
 
 describe("Test server paths", () => {
-  afterAll = () => {
-    mongoose.disconnect();
-  };
   test("GET on / path", async () => {
     const response = await request(app).get("/");
     expect(response.status).toBe(200);
